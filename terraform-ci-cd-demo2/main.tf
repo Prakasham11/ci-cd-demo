cf09173c2ae65ba3278ef5f23f029136177d9bce
Prakasham11 <arunprakash.p@sece.ac.in>
2025-07-25 12:55:46 +0530

    Add Terraform S3 bucket setup

---
+++ terraform-ci-cd-demo2/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket        = var.bucket_name
+  force_destroy = true
+
+  tags = {
+    Environment = "CI-CD"
+    Project     = "GitHubActionsDemo"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "demo_bucket_versioning" {
+  bucket = aws_s3_bucket.demo_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
